a8e446a5d73c04960451dc457a559020cc980067
Jesus Orozco <jesusorozcovaca@gmail.com>
2025-10-15 22:23:55 +0200

    saneo de archivos basura

--- produccion_latest/views/layouts/footer2.php
+++
@@ -1,6 +0,0 @@
-<footer>
-        <p>&copy; 2024 Sistema de Simulación de Contratación Pública</p>
-    </footer>
-    <script src="<?php echo js('moderator-dashboard.js'); ?>"></script>
-</body>
-</html>
--- produccion_latest/views/participant/tabs/dates_tab.php
+++
@@ -1,8 +0,0 @@
-<!-- En el archivo /views/participant/tabs/dates_tab.php -->
-
-<h3>Fechas del Proceso</h3>
-<ul>
-<?php foreach ($dates as $key => $date): ?>
-    <li><strong><?php echo $key; ?>:</strong> <?php echo $date->format('d/m/Y H:i:s'); ?></li>
-<?php endforeach; ?>
-</ul>
